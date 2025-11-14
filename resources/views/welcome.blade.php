0d71f8889e2f489b9a98fd281c044730ee540415
chrisprojs <christiananggaresta20@gmail.com>
2025-11-14 10:54:12 +0700

    fix: welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,5 @@
             @include('component.footer')
         </div>
     </body>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 </html>
